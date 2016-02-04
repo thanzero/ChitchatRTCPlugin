779f7230304bed7790d580426dcf576539569ff1
Prathan B <prathan03@gmail.com>
2016-02-04 21:38:43 +0700

    add new function

--- src/ios/ChitchatRTC.swift
+++ src/ios/ChitchatRTC.swift
@@ -10,24 +10,45 @@ import UIKit
 @objc(ChitchatRTC) class ChitchatRTC: CDVPlugin {
 
     var command = CDVInvokedUrlCommand()
-
+    var waitforEndCallCommand = CDVInvokedUrlCommand()
+    
+    let socket = SocketIOClient(socketURL: NSURL(string: "ws://203.113.25.44:3000")!)
+    
     func freeCall(command: CDVInvokedUrlCommand) {
         self.command = command
 
-        //let callerId = command.argumentAtIndex(1) as! String
-        //let contact = command.argumentAtIndex(2) as! Int
-
-        //let sourceImages = command.argumentAtIndex(0) as! [[String: AnyObject]]
+        socket.on("connect"){ data , ack in
+            print("socket on connect:: \(data)")
+        }
         
-        let storyboard = UIStoryboard(name: "Chitchat", bundle: nil)
+        socket.on("id"){ data , ack in
+            print("socket on id:: \(data)")
+            
+            let callid:String = data[0] as! String
+            
+            let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString:callid)
+            self.commandDelegate!.sendPluginResult(result, callbackId: command.callbackId)
+        }
+        
+        socket.on("message"){ data , ack in
+            print("socket on message:: \(data)")
+            
+        }
+        
+        self.socket.onAny {
+            print("socket on \($0.event):: \($0.items)")
+        }
+        
+        socket.connect()
+        
+        //let callerId = command.argumentAtIndex(0) as! String
+        //let contact = command.argumentAtIndex(1) as! Int
 
-        /*
-        let navCtrl = storyboard.instantiateViewControllerWithIdentifier("galleryNavigationController") as! UINavigationController
-        let galleryNavCtrl = navCtrl.childViewControllers[0] as! GalleryViewController
-        galleryNavCtrl.closeCallback = modalDidClose
-        galleryNavCtrl.deleteCallback = modalDidCloseToDeleteImageAtIndex
-        galleryNavCtrl.initialIndex = index
-        galleryNavCtrl.images = images
+        let storyboard = UIStoryboard(name: "Chitchat", bundle: nil)
+        
+        let voiceCallView = storyboard.instantiateViewControllerWithIdentifier("voiceCallView") as! VoiceCallViewController
+        
+        voiceCallView.hangUpCallback = hangUpCallback
         
         var presentationStyle: UIModalPresentationStyle
         if #available(iOS 8, *) {
@@ -36,10 +57,42 @@ import UIKit
             presentationStyle = .CurrentContext
         }
         viewController!.modalPresentationStyle = presentationStyle
-        navCtrl.modalPresentationStyle = presentationStyle
+        voiceCallView.modalPresentationStyle = presentationStyle
         viewController!.modalTransitionStyle = .CoverVertical
-        viewController!.presentViewController(navCtrl, animated: true, completion: nil)
-        */
+        viewController!.presentViewController(voiceCallView, animated: true, completion: nil)
+    }
+    
+    func videoCall(command: CDVInvokedUrlCommand) {
+        self.command = command
+    }
+    
+    func waitForEndCall(command: CDVInvokedUrlCommand) {
+        self.waitforEndCallCommand = command
+    }
+    
+    func endCall(command: CDVInvokedUrlCommand) {
+        self.command = command
+        self.socket.disconnect()
+        
+        print("on endcall from callee")
+        
+        viewController!.presentingViewController?.dismissViewControllerAnimated(false, completion: nil)
+        
+        let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString:"endCall")
+        self.commandDelegate!.sendPluginResult(result, callbackId: self.waitforEndCallCommand.callbackId)
+    }
+    
+    func lineBusy(command: CDVInvokedUrlCommand) {
+        self.command = command
+    }
+    
+    func hangUpCallback(){
+        
+        self.socket.disconnect()
+        
+        let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString:"endCall")
+        self.commandDelegate!.sendPluginResult(result, callbackId: self.waitforEndCallCommand.callbackId)
+        
     }
 
 }
--- src/ios/VoiceCallViewController.swift
+++ src/ios/VoiceCallViewController.swift
@@ -12,21 +12,20 @@ import UIKit
 class VoiceCallViewController: UIViewController {
     
     
-    var closeCallback: (() -> Void)?
+    var hangUpCallback: (() -> Void)?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let audioSession:AVAudioSession = AVAudioSession.sharedInstance()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func Close(sender: UIButton) {
-        
-        dismissViewControllerAnimated(true, completion: closeCallback)
-        
+    @IBAction func hangUp(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: hangUpCallback)
     }
 }
